9403e0b32f9b18f8a7eb540c8466bcef3c93c9a0
jgonmor <77663116+jgonmor@users.noreply.github.com>
2021-01-21 11:04:23 +0100

    intento rama

--- git/src/gitClase/gitClase.java
+++ git/src/gitClase/gitClase.java
@@ -5,7 +5,7 @@ public class gitClase {
 	public static void main(String[] args) {
 
 		System.out.println("adios");
-		System.out.println("cambio en otro lado");
+		System.out.println("cambio en otro lado2");
 		
 		
 	}
